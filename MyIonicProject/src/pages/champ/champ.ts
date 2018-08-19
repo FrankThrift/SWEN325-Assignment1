7c38c2d3fabf8e6555838fe5004209dc3e2fc521
Frank Thrift <franky.thrift@gmail.com>
2018-08-19 12:00:47 +1200

    Ability Description Done!

--- MyIonicProject/src/pages/champ/champ.ts
+++ MyIonicProject/src/pages/champ/champ.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { AlertController } from 'ionic-angular';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { RestProvider } from '../../providers/rest/rest';
@@ -23,7 +24,8 @@ export class ChampPage {
   championName = 'deafult';
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-  private restProvider: RestProvider, private globals: GlobalsProvider
+  private restProvider: RestProvider, private globals: GlobalsProvider,
+  public alertCtrl: AlertController
   ) {
     this.setup();
     this.championName = globals.championSelected;
@@ -52,15 +54,46 @@ export class ChampPage {
           this.abilities.eDes = this.responseObject.data[champion].spells[2].description,
           this.abilities.r = this.responseObject.data[champion].spells[3].image.full,
           this.abilities.rDes = this.responseObject.data[champion].spells[3].description
-
-
-
       }
       //console.log(this.skins);
       console.log(this.abilities);
-      console.log(this.abilities.q);
     });
   }
+
+  showQAlert(){
+    const alert = this.alertCtrl.create({
+      title: this.championName + "'s Q'",
+      subTitle: this.abilities.qDes,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+  showWAlert(){
+    const alert = this.alertCtrl.create({
+      title: this.championName + "'s W",
+      subTitle: this.abilities.wDes,
+      buttons: ['OK']
+      });
+      alert.present();
+  }
+  showEAlert(){
+    const alert = this.alertCtrl.create({
+      title: this.championName + "'s E",
+      subTitle: this.abilities.eDes,
+      buttons: ['OK']
+      });
+      alert.present();
+  }
+  showRAlert(){
+    const alert = this.alertCtrl.create({
+      title: this.championName + "'s Ultimate",
+      subTitle: this.abilities.rDes,
+      buttons: ['OK']
+      });
+      alert.present();
+  }
+
+
 
 }
 //<img src ="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/{{championName}}_{{skin.num}}.jpg.png" item-center>
